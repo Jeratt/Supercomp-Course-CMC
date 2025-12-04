79e25ab6051cda338c3cb059de61150116188c9c
Sazonov Georgy <gera950@inbox.ru>
2025-12-05 01:33:56 +0300

    new version MPI

--- solution.cpp
+++ solution.cpp
@@ -5,11 +5,12 @@
 #include <cmath>
 #include <mpi.h>
 
-// Обмен призрачными слоями между соседними процессами
+// Функция для обмена призрачными слоями
 void exchange_ghost_layers(Block& b, VDOUB& ui_local, MPI_Comm& comm_cart) {
     MPI_Request reqs[12];
     int req_count = 0;
     
+    // 0: left (y-), 1: right (y+), 2: bottom (x-), 3: top (x+), 4: front (z-), 5: back (z+)
     for (int dir = 0; dir < 6; ++dir) {
         int send_neighbor = b.neighbors[dir];
         int recv_neighbor = b.neighbors[(dir % 2 == 0) ? (dir + 1) : (dir - 1)];
@@ -33,17 +34,59 @@ void exchange_ghost_layers(Block& b, VDOUB& ui_local, MPI_Comm& comm_cart) {
                     data_size = 0;
             }
             
-            if (dir % 2 == 0) { // send from lower boundary
-                MPI_Isend(&ui_local[b.index(1, 1, 1)], data_size, MPI_DOUBLE, send_neighbor, b.rank * 6 + dir, comm_cart, &reqs[req_count++]);
-            } else { // send from upper boundary
-                MPI_Isend(&ui_local[b.index(b.padded_Nx-2, 1, 1)], data_size, MPI_DOUBLE, send_neighbor, b.rank * 6 + dir, comm_cart, &reqs[req_count++]);
+            // Отправка данных
+            if (dir % 2 == 0) { // Отправка с нижней границы
+                switch(dir) {
+                    case 0: // y-
+                        MPI_Isend(&ui_local[b.index(1, 1, 1)], data_size, MPI_DOUBLE, send_neighbor, 0, comm_cart, &reqs[req_count++]);
+                        break;
+                    case 2: // x-
+                        MPI_Isend(&ui_local[b.index(1, 1, 1)], data_size, MPI_DOUBLE, send_neighbor, 2, comm_cart, &reqs[req_count++]);
+                        break;
+                    case 4: // z-
+                        MPI_Isend(&ui_local[b.index(1, 1, 1)], data_size, MPI_DOUBLE, send_neighbor, 4, comm_cart, &reqs[req_count++]);
+                        break;
+                }
+            } else { // Отправка с верхней границы
+                switch(dir) {
+                    case 1: // y+
+                        MPI_Isend(&ui_local[b.index(1, b.Ny, 1)], data_size, MPI_DOUBLE, send_neighbor, 1, comm_cart, &reqs[req_count++]);
+                        break;
+                    case 3: // x+
+                        MPI_Isend(&ui_local[b.index(b.Nx, 1, 1)], data_size, MPI_DOUBLE, send_neighbor, 3, comm_cart, &reqs[req_count++]);
+                        break;
+                    case 5: // z+
+                        MPI_Isend(&ui_local[b.index(1, 1, b.Nz)], data_size, MPI_DOUBLE, send_neighbor, 5, comm_cart, &reqs[req_count++]);
+                        break;
+                }
             }
             
+            // Прием данных
             if (recv_neighbor != MPI_PROC_NULL) {
-                if (dir % 2 == 0) { // receive to upper ghost layer
-                    MPI_Irecv(&ui_local[b.index(b.padded_Nx-1, 1, 1)], data_size, MPI_DOUBLE, recv_neighbor, recv_neighbor * 6 + (dir+1), comm_cart, &reqs[req_count++]);
-                } else { // receive to lower ghost layer
-                    MPI_Irecv(&ui_local[b.index(0, 1, 1)], data_size, MPI_DOUBLE, recv_neighbor, recv_neighbor * 6 + (dir-1), comm_cart, &reqs[req_count++]);
+                if (dir % 2 == 0) { // Прием на верхний призрачный слой
+                    switch(dir) {
+                        case 0: // y-
+                            MPI_Irecv(&ui_local[b.index(1, b.Ny+1, 1)], data_size, MPI_DOUBLE, recv_neighbor, 1, comm_cart, &reqs[req_count++]);
+                            break;
+                        case 2: // x-
+                            MPI_Irecv(&ui_local[b.index(b.Nx+1, 1, 1)], data_size, MPI_DOUBLE, recv_neighbor, 3, comm_cart, &reqs[req_count++]);
+                            break;
+                        case 4: // z-
+                            MPI_Irecv(&ui_local[b.index(1, 1, b.Nz+1)], data_size, MPI_DOUBLE, recv_neighbor, 5, comm_cart, &reqs[req_count++]);
+                            break;
+                    }
+                } else { // Прием на нижний призрачный слой
+                    switch(dir) {
+                        case 1: // y+
+                            MPI_Irecv(&ui_local[b.index(1, 0, 1)], data_size, MPI_DOUBLE, recv_neighbor, 0, comm_cart, &reqs[req_count++]);
+                            break;
+                        case 3: // x+
+                            MPI_Irecv(&ui_local[b.index(0, 1, 1)], data_size, MPI_DOUBLE, recv_neighbor, 2, comm_cart, &reqs[req_count++]);
+                            break;
+                        case 5: // z+
+                            MPI_Irecv(&ui_local[b.index(1, 1, 0)], data_size, MPI_DOUBLE, recv_neighbor, 4, comm_cart, &reqs[req_count++]);
+                            break;
+                    }
                 }
             }
         }
@@ -54,49 +97,49 @@ void exchange_ghost_layers(Block& b, VDOUB& ui_local, MPI_Comm& comm_cart) {
 }
 
 // Заполнение буферов для обмена данными
-void fill_send_buffers(Block& b, VDOUB& ui_local, VDOUB& send_buf, int dir) {
+void fill_send_buffers(Block& b, VDOUB& ui_local, int dir) {
     int idx = 0;
     
     switch(dir) {
         case 0: // left (y-)
             for (int i = 1; i <= b.Nx; ++i) {
                 for (int k = 1; k <= b.Nz; ++k) {
-                    send_buf[idx++] = ui_local[b.index(i, 1, k)];
+                    b.left_send[idx++] = ui_local[b.index(i, 1, k)];
                 }
             }
             break;
         case 1: // right (y+)
             for (int i = 1; i <= b.Nx; ++i) {
                 for (int k = 1; k <= b.Nz; ++k) {
-                    send_buf[idx++] = ui_local[b.index(i, b.Ny, k)];
+                    b.right_send[idx++] = ui_local[b.index(i, b.Ny, k)];
                 }
             }
             break;
         case 2: // bottom (x-)
             for (int j = 1; j <= b.Ny; ++j) {
                 for (int k = 1; k <= b.Nz; ++k) {
-                    send_buf[idx++] = ui_local[b.index(1, j, k)];
+                    b.bottom_send[idx++] = ui_local[b.index(1, j, k)];
                 }
             }
             break;
         case 3: // top (x+)
             for (int j = 1; j <= b.Ny; ++j) {
                 for (int k = 1; k <= b.Nz; ++k) {
-                    send_buf[idx++] = ui_local[b.index(b.Nx, j, k)];
+                    b.top_send[idx++] = ui_local[b.index(b.Nx, j, k)];
                 }
             }
             break;
         case 4: // front (z-)
             for (int i = 1; i <= b.Nx; ++i) {
                 for (int j = 1; j <= b.Ny; ++j) {
-                    send_buf[idx++] = ui_local[b.index(i, j, 1)];
+                    b.front_send[idx++] = ui_local[b.index(i, j, 1)];
                 }
             }
             break;
         case 5: // back (z+)
             for (int i = 1; i <= b.Nx; ++i) {
                 for (int j = 1; j <= b.Ny; ++j) {
-                    send_buf[idx++] = ui_local[b.index(i, j, b.Nz)];
+                    b.back_send[idx++] = ui_local[b.index(i, j, b.Nz)];
                 }
             }
             break;
@@ -104,56 +147,56 @@ void fill_send_buffers(Block& b, VDOUB& ui_local, VDOUB& send_buf, int dir) {
 }
 
 // Заполнение призрачных слоев из полученных данных
-void fill_ghost_layers(Block& b, VDOUB& ui_local, VDOUB& recv_buf, int dir) {
+void fill_ghost_layers(Block& b, VDOUB& ui_local, int dir) {
     int idx = 0;
     
     switch(dir) {
         case 0: // left (y-)
             for (int i = 1; i <= b.Nx; ++i) {
                 for (int k = 1; k <= b.Nz; ++k) {
-                    ui_local[b.index(i, 0, k)] = recv_buf[idx++];
+                    ui_local[b.index(i, 0, k)] = b.left_recieve[idx++];
                 }
             }
             break;
         case 1: // right (y+)
             for (int i = 1; i <= b.Nx; ++i) {
                 for (int k = 1; k <= b.Nz; ++k) {
-                    ui_local[b.index(i, b.Ny+1, k)] = recv_buf[idx++];
+                    ui_local[b.index(i, b.Ny+1, k)] = b.right_recieve[idx++];
                 }
             }
             break;
         case 2: // bottom (x-)
             for (int j = 1; j <= b.Ny; ++j) {
                 for (int k = 1; k <= b.Nz; ++k) {
-                    ui_local[b.index(0, j, k)] = recv_buf[idx++];
+                    ui_local[b.index(0, j, k)] = b.bottom_recieve[idx++];
                 }
             }
             break;
         case 3: // top (x+)
             for (int j = 1; j <= b.Ny; ++j) {
                 for (int k = 1; k <= b.Nz; ++k) {
-                    ui_local[b.index(b.Nx+1, j, k)] = recv_buf[idx++];
+                    ui_local[b.index(b.Nx+1, j, k)] = b.top_recieve[idx++];
                 }
             }
             break;
         case 4: // front (z-)
             for (int i = 1; i <= b.Nx; ++i) {
                 for (int j = 1; j <= b.Ny; ++j) {
-                    ui_local[b.index(i, j, 0)] = recv_buf[idx++];
+                    ui_local[b.index(i, j, 0)] = b.front_recieve[idx++];
                 }
             }
             break;
         case 5: // back (z+)
             for (int i = 1; i <= b.Nx; ++i) {
                 for (int j = 1; j <= b.Ny; ++j) {
-                    ui_local[b.index(i, j, b.Nz+1)] = recv_buf[idx++];
+                    ui_local[b.index(i, j, b.Nz+1)] = b.back_recieve[idx++];
                 }
             }
             break;
     }
 }
 
-// Адаптированный оператор Лапласа для локального блока
+// Корректный оператор Лапласа для локального блока
 inline double laplace_operator(const Grid& g, const Block& b, const VDOUB& ui_local, const int& i, const int& j, const int& k) {
     return (ui_local[b.index(i-1, j, k)] - 2 * ui_local[b.index(i, j, k)] + ui_local[b.index(i+1, j, k)]) / (g.h_x * g.h_x) +
            (ui_local[b.index(i, j-1, k)] - 2 * ui_local[b.index(i, j, k)] + ui_local[b.index(i, j+1, k)]) / (g.h_y * g.h_y) +
@@ -164,74 +207,7 @@ inline double laplace_operator(const Grid& g, const Block& b, const VDOUB& ui_lo
 void init(const Grid& g, Block& b, VVEC& u_local, const int& dim0_n, const int& dim1_n, const int& dim2_n, 
          MPI_Comm& comm_cart, double& max_inaccuracy, double& first_step_inaccuracy) {
     
-    // Установка значений в призрачных слоях для периодических условий по y
-    if (b.dim0_rank == 0) {
-        // Нижняя граница по y (y=0)
-        for (int i = 1; i <= b.Nx; ++i) {
-            for (int k = 1; k <= b.Nz; ++k) {
-                double x = (b.x_start + i - 1) * g.h_x;
-                double z = (b.z_start + k - 1) * g.h_z;
-                u_local[0][b.index(i, 0, k)] = u_analytical(g, x, 0.0, z, 0.0);
-                u_local[1][b.index(i, 0, k)] = u_analytical(g, x, 0.0, z, g.tau);
-            }
-        }
-    }
-    
-    if (b.dim0_rank == dim0_n - 1) {
-        // Верхняя граница по y (y=Ly)
-        for (int i = 1; i <= b.Nx; ++i) {
-            for (int k = 1; k <= b.Nz; ++k) {
-                double x = (b.x_start + i - 1) * g.h_x;
-                double z = (b.z_start + k - 1) * g.h_z;
-                u_local[0][b.index(i, b.Ny+1, k)] = u_analytical(g, x, g.Ly, z, 0.0);
-                u_local[1][b.index(i, b.Ny+1, k)] = u_analytical(g, x, g.Ly, z, g.tau);
-            }
-        }
-    }
-    
-    // Условия первого рода для x=0 и x=Lx
-    if (b.dim1_rank == 0) {
-        // x=0 граница
-        for (int j = 0; j <= b.Ny+1; ++j) {
-            for (int k = 0; k <= b.Nz+1; ++k) {
-                u_local[0][b.index(0, j, k)] = 0.0;
-                u_local[1][b.index(0, j, k)] = 0.0;
-            }
-        }
-    }
-    
-    if (b.dim1_rank == dim1_n - 1) {
-        // x=Lx граница
-        for (int j = 0; j <= b.Ny+1; ++j) {
-            for (int k = 0; k <= b.Nz+1; ++k) {
-                u_local[0][b.index(b.Nx+1, j, k)] = 0.0;
-                u_local[1][b.index(b.Nx+1, j, k)] = 0.0;
-            }
-        }
-    }
-    
-    // Условия первого рода для z=0 и z=Lz
-    if (b.dim2_rank == 0) {
-        // z=0 граница
-        for (int i = 0; i <= b.Nx+1; ++i) {
-            for (int j = 0; j <= b.Ny+1; ++j) {
-                u_local[0][b.index(i, j, 0)] = 0.0;
-                u_local[1][b.index(i, j, 0)] = 0.0;
-            }
-        }
-    }
-    
-    if (b.dim2_rank == dim2_n - 1) {
-        // z=Lz граница
-        for (int i = 0; i <= b.Nx+1; ++i) {
-            for (int j = 0; j <= b.Ny+1; ++j) {
-                u_local[0][b.index(i, j, b.Nz+1)] = 0.0;
-                u_local[1][b.index(i, j, b.Nz+1)] = 0.0;
-            }
-        }
-    }
-    
-    // Внутренние точки для u^0 (начальное условие)
+    // Инициализация всех точек u^0 аналитическим решением
     for (int i = 1; i <= b.Nx; ++i) {
         for (int j = 1; j <= b.Ny; ++j) {
             for (int k = 1; k <= b.Nz; ++k) {
@@ -239,6 +215,64 @@ void init(const Grid& g, Block& b, VVEC& u_local, const int& dim0_n, const int& 
                 double y = (b.y_start + j - 1) * g.h_y;
                 double z = (b.z_start + k - 1) * g.h_z;
                 u_local[0][b.index(i, j, k)] = u_analytical(g, x, y, z, 0.0);
+            }
+        }
+    }
+    
+    // Граничные условия для u^0
+    // Условия первого рода на x=0 и x=Lx
+    
+    // Установка значений для границ x=0 и x=Lx
+    if (b.dim1_rank == 0) {
+        for (int j = 1; j <= b.Ny; ++j) {
+            for (int k = 1; k <= b.Nz; ++k) {
+                u_local[0][b.index(0, j, k)] = 0.0;
+            }
+        }
+    }
+    
+    if (b.dim1_rank == dim1_n - 1) {
+        for (int j = 1; j <= b.Ny; ++j) {
+            for (int k = 1; k <= b.Nz; ++k) {
+                u_local[0][b.index(b.Nx+1, j, k)] = 0.0;
+            }
+        }
+    }
+    
+    // Условия первого рода на z=0 и z=Lz
+    if (b.dim2_rank == 0) {
+        for (int i = 1; i <= b.Nx; ++i) {
+            for (int j = 1; j <= b.Ny; ++j) {
+                u_local[0][b.index(i, j, 0)] = 0.0;
+            }
+        }
+    }
+    
+    if (b.dim2_rank == dim2_n - 1) {
+        for (int i = 1; i <= b.Nx; ++i) {
+            for (int j = 1; j <= b.Ny; ++j) {
+                u_local[0][b.index(i, j, b.Nz+1)] = 0.0;
+            }
+        }
+    }
+    
+    // Периодические условия по y
+    if (b.dim0_rank == 0) {
+        for (int i = 1; i <= b.Nx; ++i) {
+            for (int k = 1; k <= b.Nz; ++k) {
+                double x = (b.x_start + i - 1) * g.h_x;
+                double z = (b.z_start + k - 1) * g.h_z;
+                u_local[0][b.index(i, 0, k)] = u_analytical(g, x, 0.0, z, 0.0);
+            }
+        }
+    }
+    
+    if (b.dim0_rank == dim0_n - 1) {
+        for (int i = 1; i <= b.Nx; ++i) {
+            for (int k = 1; k <= b.Nz; ++k) {
+                double x = (b.x_start + i - 1) * g.h_x;
+                double z = (b.z_start + k - 1) * g.h_z;
+                u_local[0][b.index(i, b.Ny+1, k)] = u_analytical(g, x, g.Ly, z, 0.0);
             }
         }
     }
@@ -256,7 +290,39 @@ void init(const Grid& g, Block& b, VVEC& u_local, const int& dim0_n, const int& 
         }
     }
     
-    // Граничные точки для u^1
+    // Граничные условия для u^1
+    if (b.dim1_rank == 0) {
+        for (int j = 0; j <= b.Ny+1; ++j) {
+            for (int k = 0; k <= b.Nz+1; ++k) {
+                u_local[1][b.index(0, j, k)] = 0.0;
+            }
+        }
+    }
+    
+    if (b.dim1_rank == dim1_n - 1) {
+        for (int j = 0; j <= b.Ny+1; ++j) {
+            for (int k = 0; k <= b.Nz+1; ++k) {
+                u_local[1][b.index(b.Nx+1, j, k)] = 0.0;
+            }
+        }
+    }
+    
+    if (b.dim2_rank == 0) {
+        for (int i = 0; i <= b.Nx+1; ++i) {
+            for (int j = 0; j <= b.Ny+1; ++j) {
+                u_local[1][b.index(i, j, 0)] = 0.0;
+            }
+        }
+    }
+    
+    if (b.dim2_rank == dim2_n - 1) {
+        for (int i = 0; i <= b.Nx+1; ++i) {
+            for (int j = 0; j <= b.Ny+1; ++j) {
+                u_local[1][b.index(i, j, b.Nz+1)] = 0.0;
+            }
+        }
+    }
+    
     // Периодические условия по y
     if (b.dim0_rank == 0) {
         for (int i = 1; i <= b.Nx; ++i) {
@@ -318,7 +384,7 @@ void run_algo(const Grid& g, Block& b, VVEC& u_local, const int& dim0_n, const i
         // Обмен призрачными слоями для текущего временного слоя
         exchange_ghost_layers(b, u_local[curr], comm_cart);
         
-        // Обработка внутренних точек
+        // Вычисление значений для внутренних точек
         for (int i = 1; i <= b.Nx; ++i) {
             for (int j = 1; j <= b.Ny; ++j) {
                 for (int k = 1; k <= b.Nz; ++k) {
@@ -329,7 +395,41 @@ void run_algo(const Grid& g, Block& b, VVEC& u_local, const int& dim0_n, const i
             }
         }
         
-        // Граничные условия
+        // Граничные условия для следующего временного слоя
+        
+        // Условия первого рода на x=0 и x=Lx
+        if (b.dim1_rank == 0) {
+            for (int j = 0; j <= b.Ny+1; ++j) {
+                for (int k = 0; k <= b.Nz+1; ++k) {
+                    u_local[next][b.index(0, j, k)] = 0.0;
+                }
+            }
+        }
+        
+        if (b.dim1_rank == dim1_n - 1) {
+            for (int j = 0; j <= b.Ny+1; ++j) {
+                for (int k = 0; k <= b.Nz+1; ++k) {
+                    u_local[next][b.index(b.Nx+1, j, k)] = 0.0;
+                }
+            }
+        }
+        
+        // Условия первого рода на z=0 и z=Lz
+        if (b.dim2_rank == 0) {
+            for (int i = 0; i <= b.Nx+1; ++i) {
+                for (int j = 0; j <= b.Ny+1; ++j) {
+                    u_local[next][b.index(i, j, 0)] = 0.0;
+                }
+            }
+        }
+        
+        if (b.dim2_rank == dim2_n - 1) {
+            for (int i = 0; i <= b.Nx+1; ++i) {
+                for (int j = 0; j <= b.Ny+1; ++j) {
+                    u_local[next][b.index(i, j, b.Nz+1)] = 0.0;
+                }
+            }
+        }
         
         // Периодические условия по y
         if (b.dim0_rank == 0) {
@@ -354,41 +454,10 @@ void run_algo(const Grid& g, Block& b, VVEC& u_local, const int& dim0_n, const i
             }
         }
         
-        // Условия первого рода для x
-        if (b.dim1_rank == 0) {
-            for (int j = 0; j <= b.Ny+1; ++j) {
-                for (int k = 0; k <= b.Nz+1; ++k) {
-                    u_local[next][b.index(0, j, k)] = 0.0;
-                }
-            }
-        }
+        // Обмен призрачными слоями для следующего шага
+        exchange_ghost_layers(b, u_local[next], comm_cart);
         
-        if (b.dim1_rank == dim1_n - 1) {
-            for (int j = 0; j <= b.Ny+1; ++j) {
-                for (int k = 0; k <= b.Nz+1; ++k) {
-                    u_local[next][b.index(b.Nx+1, j, k)] = 0.0;
-                }
-            }
-        }
-        
-        // Условия первого рода для z
-        if (b.dim2_rank == 0) {
-            for (int i = 0; i <= b.Nx+1; ++i) {
-                for (int j = 0; j <= b.Ny+1; ++j) {
-                    u_local[next][b.index(i, j, 0)] = 0.0;
-                }
-            }
-        }
-        
-        if (b.dim2_rank == dim2_n - 1) {
-            for (int i = 0; i <= b.Nx+1; ++i) {
-                for (int j = 0; j <= b.Ny+1; ++j) {
-                    u_local[next][b.index(i, j, b.Nz+1)] = 0.0;
-                }
-            }
-        }
-        
-        // Расчет погрешности на текущем шаге
+        // Расчет погрешности
         double local_max_error = 0.0;
         for (int i = 1; i <= b.Nx; ++i) {
             for (int j = 1; j <= b.Ny; ++j) {
