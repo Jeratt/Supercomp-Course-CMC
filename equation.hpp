316864466ede22fadcf956d79c593e8c47138bd8
Sazonov Georgy <gera950@inbox.ru>
2025-12-06 00:33:42 +0300

    new version MPI

--- equation.hpp
+++ equation.hpp
@@ -39,4 +39,56 @@ void solve_mpi(const Grid& g, Block& b,
                double& first_step_inaccuracy,
                double& last_step_inaccuracy,
                VDOUB& result);
+
+class Block {
+public:
+    int rank, x_start, x_end, y_start, y_end, z_start, z_end;
+    int Nx, Ny, Nz, padded_Nx, padded_Ny, padded_Nz;
+    vector<int> neighbors;
+    vector<double> left_send, left_recv, right_send, right_recv;
+    vector<double> bottom_send, bottom_recv, top_send, top_recv;
+    vector<double> front_send, front_recv, back_send, back_recv;
+
+    Block(const Grid& g, const vector<int>& nb, const int coords[3], 
+          int dimx, int dimy, int dimz, int r) : neighbors(nb), rank(r) {
+        // Расчет локальных границ блока
+        x_start = coords[0] * (g.N / dimx);
+        x_end = (coords[0] + 1) * (g.N / dimx);
+        if (coords[0] == dimx - 1) x_end = g.N;
+        
+        y_start = coords[1] * (g.N / dimy);
+        y_end = (coords[1] + 1) * (g.N / dimy);
+        if (coords[1] == dimy - 1) y_end = g.N;
+        
+        z_start = coords[2] * (g.N / dimz);
+        z_end = (coords[2] + 1) * (g.N / dimz);
+        if (coords[2] == dimz - 1) z_end = g.N;
+        
+        Nx = x_end - x_start;
+        Ny = y_end - y_start;
+        Nz = z_end - z_start;
+        
+        // Размеры с учетом гало-зон (1 слой с каждой стороны)
+        padded_Nx = Nx + 2;
+        padded_Ny = Ny + 2;
+        padded_Nz = Nz + 2;
+        
+        // Инициализация буферов для обмена гало-зонами
+        int yz_size = Ny * Nz;
+        int xz_size = Nx * Nz;
+        int xy_size = Nx * Ny;
+        
+        left_send.resize(yz_size); left_recv.resize(yz_size);
+        right_send.resize(yz_size); right_recv.resize(yz_size);
+        bottom_send.resize(xz_size); bottom_recv.resize(xz_size);
+        top_send.resize(xz_size); top_recv.resize(xz_size);
+        front_send.resize(xy_size); front_recv.resize(xy_size);
+        back_send.resize(xy_size); back_recv.resize(xy_size);
+    }
+    
+    inline int local_index(int i, int j, int k) const {
+        return ((i) * padded_Ny + (j)) * padded_Nz + (k);
+    }
+};
+
 #endif
